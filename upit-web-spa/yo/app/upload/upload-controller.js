cd3e25cbef08a1b6bb3c9c57580e34d83919d2ac
josh williford <josh@jbw.io>
2015-05-13 02:11:07 -0700

    updated to 1.4 beta (angular) and made upload page use ng-file-upload instead

--- upit-web-spa/yo/app/upload/upload-controller.js
+++ upit-web-spa/yo/app/upload/upload-controller.js
@@ -8,60 +8,31 @@
  * Controller of upit
  */
 angular.module('upitWebSpa.upload')
-    .controller('UploadCtrl', ['$scope', '$window', 'FileUploader', 'FileUrlGenerator', function ($scope, $window, FileUploader, FileUrlGenerator) {
+  .controller('UpCtrl', ['$scope', '$window', 'Upload', 'FileUrlGenerator', function ($scope, $window, Upload, FileUrlGenerator) {
+    $scope.$watch('files', function () {
+      $scope.upload($scope.files);
+    });
+    $scope.log = '';
 
-        var lastUploadTrackerId = 0;
-
-        var model = {
-            urlsToDownload: [ ],
-            uploads: [ ]
-        };
-
-        var TrackedUpload = function(id, theFileItem) {
-            this.id = id,
-            this.fileItem = theFileItem,
-            this.uploadedFile  = null,
-            this.urls = {
-
-            }
-        };
-
-        var fileUploader = new FileUploader({
+    $scope.upload = function (files) {
+      if (files && files.length) {
+        for (var i = 0; i < files.length; i++) {
+          var file = files[i];
+          file.index = i;
+          Upload.upload({
             url: '/api_v1/uploadedFile/upload',
-            removeAfterUpload: true
-        });
-
-        fileUploader.onAfterAddingFile = function(fileItem) {
-            var trackedUpload = new TrackedUpload(lastUploadTrackerId++, fileItem);
-            model.uploads.push(trackedUpload);
-
-            trackedUpload.fileItem.onSuccess = function(response, status, headers) {
-                // TODO: Validation?
-                trackedUpload.uploadedFile = response[0];
-                angular.copy(FileUrlGenerator.getURLs(trackedUpload.uploadedFile), trackedUpload.urls);
-            };
-
-        };
-
-        $scope.model = model;
-        $scope.fileUploader = fileUploader;
-
-        $scope.removeFile = function(upload) {
-            fileUploader.removeFromQueue(upload.fileItem);
+            file: file
+          }).progress(function (evt) {
+            var progressPercentage = parseInt(100.0 * evt.loaded / evt.total);
+            var fileInProgress = $scope.files[evt.config.file.index];
+            if (fileInProgress) {fileInProgress.progress = progressPercentage};
+          }).success(function (data, status, headers, config) {
+            var completedFile = $scope.files[config.file.index];
+            completedFile.urls = {};
+            angular.copy(FileUrlGenerator.getURLs(data[0]), completedFile.urls);
+            completedFile.uploadedFile = true;
+          });
         }
-
-        $scope.uploadFile = function(fileItem) {
-            fileItem.upload();
-        };
-
-        $scope.removeAll = function() {
-            fileUploader.cancelAll();
-            fileUploader.clearQueue();
-            model.uploads.length = 0;
-        };
-
-        $scope.uploadAll = function() {
-            fileUploader.uploadAll();
-        };
-
-    }]);
+      }
+    };
+  }]);
--- upit-web-spa/yo/app/upload/upload.js
+++ upit-web-spa/yo/app/upload/upload.js
@@ -5,29 +5,25 @@
  * @name upit.controller:MainCtrl
  * @description # Paste Module
  */
-angular.module('upitWebSpa.upload', [
-    'ngRoute',
-    'angularFileUpload'
-  ])
+angular.module('upitWebSpa.upload', ['ngFileUpload'])
   .config(['$routeProvider', function ($routeProvider) {
 
     $routeProvider.when('/upload', {
-        templateUrl: 'upload/upload.html',
-        controller: 'UploadCtrl'
+      templateUrl: 'upload/upload.html',
+      controller: 'UpCtrl'
     })
-    .when('/file/:fileIdHash/:fileName?', {
-            templateUrl: 'upload/file.html',
-            controller: 'FileCtrl',
-            resolve: {
-                resolvedUploadedFile: ['$route', 'UploadedFileResource', function($route, UploadedFileResource) {
-                    // Conditionally resolve the uploaded file by the ID in the URL
-                    if($route.current.params.fileIdHash) {
-                        return UploadedFileResource.getByIdHash($route.current.params.fileIdHash);
-                    }
-                    // TODO: If the resolve fails we need to alert the user?
-                    return null;
-                }]
+      .when('/file/:fileIdHash/:fileName?', {
+        templateUrl: 'upload/file.html',
+        controller: 'FileCtrl',
+        resolve: {
+          resolvedUploadedFile: ['$route', 'UploadedFileResource', function($route, UploadedFileResource) {
+            // Conditionally resolve the uploaded file by the ID in the URL
+            if($route.current.params.fileIdHash) {
+              return UploadedFileResource.getByIdHash($route.current.params.fileIdHash);
             }
-        });
-
+            // TODO: If the resolve fails we need to alert the user?
+            return null;
+          }]
+        }
+      });
   }]);
